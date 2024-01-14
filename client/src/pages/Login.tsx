aeec36adfee2a952926300797142d1d8aa67ef21
Starboy-Sharma <pankajsharmavats88@gmail.com>
2024-01-15 00:02:52 +0530

    init login design layout

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,25 @@
-import { Button } from '@chakra-ui/react';
+import {
+  Container,
+  Heading,
+  Icon,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+} from '@chakra-ui/react';
+import { FaUser } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import { authContext } from '../hooks/useAuth';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+
+  const handleInputChange = (e) => setEmail(e.target.value);
+
+  const isError = email === '';
+
   const navigate = useNavigate();
   const { login } = useContext(authContext);
 
@@ -16,11 +32,37 @@ const Login = () => {
 
   return (
     <>
-      <div>Login</div>
-      <Button colorScheme="blue" onClick={handleLogin}>
-        {' '}
-        Login{' '}
-      </Button>
+      <div className="wrapper">
+        <div className="box cover-img"></div>
+        <div className="box">
+          <Container>
+            <Heading pt="2rem" as="h1" size="lg">
+              {' '}
+              <Icon
+                as={FaUser}
+                p={'5px'}
+                className="user-icon"
+                color={'white'}
+                w="25px"
+                h="25px"
+              />{' '}
+              Welcome back{' '}
+            </Heading>
+
+            <FormControl isInvalid={isError}>
+              <FormLabel>Email</FormLabel>
+              <Input type="email" value={email} onChange={handleInputChange} />
+              {!isError ? (
+                <FormHelperText>
+                  Enter the email.
+                </FormHelperText>
+              ) : (
+                <FormErrorMessage>Email is required.</FormErrorMessage>
+              )}
+            </FormControl>
+          </Container>
+        </div>
+      </div>
     </>
   );
 };
